8b68f23afcecf80864f264aa6da4ca55e1ff081a
charsle <coolboy11058@126.com>
2019-01-06 17:55:50 +0800

    添加八方节点拉

---
+++ example/NormalLayout/largedata.ts
@@ -0,0 +1,21 @@
+export const Words = [
+    { content: 'You can do anything, but not everything.' },
+    { content: 'Those who dare to fail miserably can achieve greatly.' },
+    { content: 'You miss 100 percent of the shots you never take.' },
+    { content: 'Those who believe in telekinetics, raise my hand.' },
+    { content: 'I’d rather live with a good question than a bad answer.' },
+    { content: 'You can do anything, but not everything.' }
+   
+]
+
+export const SpanStyle = [
+    { cursor:'n-resize',left:'50%',top:'2px' ,marginLeft:'2px', pos:'n'},
+    { cursor:'ne-resize',top:'2px' ,right:'2px',pos:'ne'},
+    { cursor:'e-resize',top:'50%',right:'2px' ,marginTop:'2px',pos:'e'},
+    { cursor:'se-resize',bottom:'2px' ,right:'2px',pos:'se'},
+
+    { cursor:'s-resize',bottom:'2px',right:'50%' ,marginLeft:'2px',pos:'s'},
+    { cursor:'sw-resize',left:'2px',bottom:'2px' ,pos:'sw'},
+    { cursor:'w-resize',left:'2px',top:'50%' ,marginTop:'2px',pos:'w'},
+    { cursor:'nw-resize',left:'2px',top:'2px' ,pos:'nw'}
+]
